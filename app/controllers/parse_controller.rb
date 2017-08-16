0f6898782eb4cbe22093d3e54d97bf29c7d23905
Jaeuk-YOO <ryou9305@naver.com>
2017-08-17 03:44:12 +0900

    search 되는 순간 parse

--- app/controllers/parse_controller.rb
+++ app/controllers/parse_controller.rb
@@ -1,46 +1,46 @@
 class ParseController < ApplicationController
   before_action :authenticate_user!
-  def doc
-    require 'open-uri'
-    require 'json'
-    doc = Nokogiri::HTML(open("http://place.map.daum.net/main/v/8018353"), nil, 'UTF-8')
-    get_info = JSON.parse(doc.css('p').inner_html)
-    puts get_info.class
-    doc_hash = Hash.new
-    doc_hash_sel = Hash.new
-    doc_hash = doc
-
-    get_info.each do |key, value|
-      if key.include? "menuInfo"
-        puts key
-        puts value
-      end
-    end
-    @doc_css= doc.css('p').inner_html
+  def parse_view
+    @pp = ParsePlace.all
   end
 
   def search_create
-    us = UserSearch.new
-    us.user_id = current_user.id
-    us.email = current_user.email
-    us.place_name = params[:place_name]
-    us.address_name = params[:address_name]
-    us.place_id = params[:place_id]
-    us.place_x = params[:place_x]
-    us.place_y = params[:place_y]
-    us.place_phone = params[:place_phone]
-    us.save
-#    us = Hash.new
-#    us['user_id'] = current_user.id
-#    us['email'] = current_user.email
-#    us['place_name'] = params[:place_name]
-#    us['address_name'] = params[:address_name]
-#    us['place_id'] = params[:place_id]
-#    us['place_x'] = params[:place_x]
-#    us['place_y'] = params[:place_y]
-#    us['place_phone'] = params[:place_phone]
+    if UserSearch.where(user_id: current_user.id).where(place_id: params[:place_id]).take.nil?
+      us = UserSearch.new
+      us.user_id = current_user.id
+      us.email = current_user.email
+      us.place_name = params[:place_name]
+      us.address_name = params[:address_name]
+      us.place_id = params[:place_id]
+      us.place_x = params[:place_x]
+      us.place_y = params[:place_y]
+      us.place_phone = params[:place_phone]
+      us.save
 
-
+      #메뉴정보 저장
+      require 'open-uri'
+      require 'json'
+      doc = Nokogiri::HTML(open("http://place.map.daum.net/main/v/"+params[:place_id]), nil, 'UTF-8')
+      get_info = JSON.parse(doc.css('p').inner_html)
+      get_info.each do |key, value|
+        if key.include? "menuInfo"
+            pp = ParsePlace.new
+            pp.menuCount = value.values_at("menucount").pop
+            pp.menuList = value.values_at("menuList").flatten
+            pp.menuboardphotourlList = value.values_at("menuboardphotourlList").flatten
+            pp.save
+        end
+      end
+    end
+    #    us = Hash.new
+    #    us['user_id'] = current_user.id
+    #    us['email'] = current_user.email
+    #    us['place_name'] = params[:place_name]
+    #    us['address_name'] = params[:address_name]
+    #    us['place_id'] = params[:place_id]
+    #    us['place_x'] = params[:place_x]
+    #    us['place_y'] = params[:place_y]
+    #    us['place_phone'] = params[:place_phone]
 
   end
 
--- db/migrate/20170815174540_create_parse_places.rb
+++ db/migrate/20170815174540_create_parse_places.rb
@@ -1,7 +1,9 @@
 class CreateParsePlaces < ActiveRecord::Migration[5.1]
   def change
     create_table :parse_places do |t|
-
+      t.string :menuCount
+      t.json :menuList
+      t.json :menuboardphotourlList
       t.timestamps
     end
   end
